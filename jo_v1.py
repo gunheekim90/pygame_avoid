9e5476473678862d026fcc6e14aefeeb156c857f
Joseph Sungpil Choi <chousemath@gmail.com>
2017-03-07 20:34:05 +0900

    added a second wall and changed the color to red

--- jo_v1.py
+++ jo_v1.py
@@ -3,8 +3,13 @@ import pygame
 # initializes all modules required for Pygame
 pygame.init()
 
+# define the window dimensions
+window_length = 400
+window_height = 300
+wall_color = (226,46,46)
+
 # return value is a Surface object for graphical operations
-screen = pygame.display.set_mode((400,300))
+screen = pygame.display.set_mode((window_length,window_height))
 done = False
 
 while not done:
@@ -12,6 +17,7 @@ while not done:
 	for event in pygame.event.get():
 		if event.type == pygame.QUIT:
 			done = True
-		pygame.draw.rect(screen,(0,128,255), pygame.Rect(30,30,60,60))
+		pygame.draw.rect(screen,wall_color, pygame.Rect(0,0,50,window_height))
+		pygame.draw.rect(screen,wall_color, pygame.Rect(window_length-50,0,50,window_height))
 	# required to show updates on the game screen
 	pygame.display.flip()
