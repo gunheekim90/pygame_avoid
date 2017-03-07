3a2f890a1542ab44404b61b9cd012c0b2af41aa7
Joseph Sungpil Choi <chousemath@gmail.com>
2017-03-07 20:25:27 +0900

    added a blue cube to the screen

--- jo_v1.py
+++ jo_v1.py
@@ -1,1 +1,17 @@
-print("hello world!")
+import pygame
+
+# initializes all modules required for Pygame
+pygame.init()
+
+# return value is a Surface object for graphical operations
+screen = pygame.display.set_mode((400,300))
+done = False
+
+while not done:
+	# event.get() empties the event queue
+	for event in pygame.event.get():
+		if event.type == pygame.QUIT:
+			done = True
+		pygame.draw.rect(screen,(0,128,255), pygame.Rect(30,30,60,60))
+	# required to show updates on the game screen
+	pygame.display.flip()
